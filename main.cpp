5a296108277d3323aed4607c2e11471884f436a6
PedroRodrigues527 <pedro.miguel.rodrigues00@gmail.com>
2023-06-07 19:01:10 +0100

    cpu flags

--- main.cpp
+++ main.cpp
@@ -2,25 +2,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 /* https://www.youtube.com/watch?v=qJgsuQoy9bc */
+
+// processor status
+struct PS 
+{
+    unsigned char : 1;
+};
+
 struct CPU
 {
     using Byte = unsigned char;  // 1 byte (8 bits)
     using Word = unsigned short; // 2 bytes (16 bits)
 
     Word ProgramCounter;
-    //TODO: if problem replace with Word
+    // TODO: if problem replace with Word
     Byte StackPointer; // supports a 256 byte stack located between $0100 and $01FF
 
-    //registers
-    Byte Accumulator; // supports a 256 byte stack located between $0100 and $01FF
-    Byte RegisterX, RegisterY;
+    // registers
+    Byte Acc; // supports a 256 byte stack located between $0100 and $01FF
+    Byte RX, RY;
 
-    Byte ProcessorStatus;
+    // flags
+    PS C; // carry
+    PS Z; // zero
+    PS I; // interrupt
+    PS D; // decimal
+    PS B; // break
+    PS O; // overflow
+    PS N; // negative
 };
 
 int main()
 {
-    std::cout << "Hello";
-
+    CPU cpu;
     return 0;
 }
